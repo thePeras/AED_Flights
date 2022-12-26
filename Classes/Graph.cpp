2cfa68c61bace1f239e9b787ad7b138cb7da2ac4
r00cha <roocha99pt@gmail.com>
2022-12-26 16:25:39 +0000

    Fixed some bugs, now you can run it

--- Classes/Graph.cpp
+++ Classes/Graph.cpp
@@ -1,12 +1,11 @@
 
 #include "Graph.h"
 
-Graph::Graph(int num, bool dir) : n(num), nodes(num+1) {}
-
 void Graph::addEdge(int src, int dest, int weight) { //TODO: change to add flight
+    bool hasDir = false;
     if (src<1 || src>n || dest<1 || dest>n) return;
 
     nodes[src].adj.push_back({dest, weight});
 
-    i f (!hasDir) nodes[dest].adj.push_back({src, weight});
+    if (!hasDir) nodes[dest].adj.push_back({src, weight});
 }
--- Classes/Graph.h
+++ Classes/Graph.h
@@ -1,13 +1,24 @@
 #pragma once
 
+#include <list>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+
 class Graph {
+
+    Graph(int num, bool dir) : n(num), nodes(num+1) {}
+
     struct Edge { //TODO: replace with Flight
         int dest;
         int weight; // this is distance or time
-    }
+    };
 
     struct Node {
-        list <Edge> adj; // only correct directed edges, flights departure from this airport
+        list<Edge> adj; // only correct directed edges, flights departure from this airport
         string airport; // airport id
     };
 
@@ -18,5 +29,5 @@ class Graph {
         Graph(int nodes);
 
         void addEdge(int src, int dest, int weight = 1);
-    };
-}
+    ;
+};
