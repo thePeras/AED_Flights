2d51eb3b51ce1b323edd31cb0329512bf7e59cc8
thePeras <jtopereira@gmail.com>
2023-01-07 11:51:46 +0000

    airports at x flights

--- Classes/Menu.cpp
+++ Classes/Menu.cpp
@@ -1,20 +1,28 @@
 #include "Menu.h"
 
-Menu::Menu(string title, string inputText, vector<string> options, vector<string> list, bool validateInput, bool isToPrintList, int nColumns) : title(title), inputText(inputText), list(list), validateInput(validateInput), isToPrintList(isToPrintList), nColumns(nColumns) {
+Menu::Menu(string title, string inputText, vector<string> options, vector<string> list, bool validateInput, bool isToPrintList, int nColumns, bool acceptIntegers) : title(title), inputText(inputText), listToPrint(list), validateInput(validateInput), isToPrintList(isToPrintList), nColumns(nColumns), acceptIntegers(acceptIntegers){
     this->listIndex = 0;
     this->options = options;
     this->optionSelected = false;
+    this->listToValidate = list;
+}
+
+Menu::Menu(string title, string inputText, vector<string> options, vector<string> list, vector<string> listToValidate, bool validateInput, bool isToPrintList, int nColumns, bool acceptIntegers) : title(title), inputText(inputText), listToPrint(list), validateInput(validateInput), isToPrintList(isToPrintList), nColumns(nColumns), acceptIntegers(acceptIntegers){
+    this->listIndex = 0;
+    this->options = options;
+    this->optionSelected = false;
+    this->listToValidate = listToValidate;
 }
 
 void Menu::showList(){
     int max = 0;
-    for (int i = 0; i < list.size(); i++) {
-        if(list[i].size() > max) max = list[i].size();
+    for (int i = 0; i < listToPrint.size(); i++) {
+        if(listToPrint[i].size() > max) max = listToPrint[i].size();
     }
 
-    for (int i = this->listIndex; i < this->listIndex + nLines * nColumns && i < list.size(); i++) {
+    for (int i = this->listIndex; i < this->listIndex + nLines * nColumns && i < listToPrint.size(); i++) {
         if(i % nColumns == 0) cout << endl;
-        cout << '\t' << left << setw(max+3) << list[i];
+        cout << '\t' << left << setw(max+3) << listToPrint[i];
     }
     cout << endl << endl;
 }
@@ -22,12 +30,13 @@ void Menu::showList(){
 bool Menu::inputIsInvalid(string input){
     try{
         int inputInt = stoi(input);
-        if(isToPrintList and list.size() > nLines * nColumns and inputInt >= 0 and inputInt <= options.size()) return false;
-        if(isToPrintList and list.size() > nLines * nColumns and inputInt >= 0 and listIndex > 0 and inputInt <= options.size() + 1) return false;
+        if(acceptIntegers) return false;
+        if(isToPrintList and listToPrint.size() > nLines * nColumns and inputInt >= 0 and inputInt <= options.size()) return false;
+        if(isToPrintList and listToPrint.size() > nLines * nColumns and inputInt >= 0 and listIndex > 0 and inputInt <= options.size() + 1) return false;
         if(inputInt >= 0 and inputInt < options.size()) return false;
-        return find(list.begin(), list.end(), input) == list.end();
+        return find(listToValidate.begin(), listToValidate.end(), input) == listToValidate.end();
     }catch (exception e){
-        return find(list.begin(), list.end(), input) == list.end();
+        return find(listToValidate.begin(), listToValidate.end(), input) == listToValidate.end();
     }
 }
 
@@ -39,7 +48,7 @@ void Menu::render(){
     for (int i = 0; i < options.size(); i++) {
         cout << '\t' << i << " - " << options[i] << endl;
     }
-    if(isToPrintList && list.size() > nLines * nColumns){
+    if(isToPrintList && listToPrint.size() > nLines * nColumns){
         cout << "\t" << options.size() << " - Próxima página" << endl;
         if(listIndex > 0) cout << "\t" << options.size() + 1 << " - Página anterior" << endl;
     }
@@ -58,12 +67,12 @@ void Menu::render(){
             optionSelected = true;
             return;
         }
-        if(isToPrintList and list.size() > nLines * nColumns and inputInt == options.size()){
+        if(isToPrintList and listToPrint.size() > nLines * nColumns and inputInt == options.size()){
             increaseListIndex();
             render();
             return;
         }
-        if(isToPrintList and list.size() > nLines * nColumns and inputInt == options.size() + 1 and listIndex > 0){
+        if(isToPrintList and listToPrint.size() > nLines * nColumns and inputInt == options.size() + 1 and listIndex > 0){
             decreaseListIndex();
             render();
             return;
@@ -87,7 +96,7 @@ bool Menu::optionIsSelected(){
 }
 
 void Menu::increaseListIndex(){
-    if(listIndex + nColumns * nLines < list.size()) listIndex += nColumns * nLines;
+    if(listIndex + nColumns * nLines < listToPrint.size()) listIndex += nColumns * nLines;
 }
 
 void Menu::decreaseListIndex(){
